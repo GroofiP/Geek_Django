f5b170c607dc62584f911d2fc5c0b4a651c6978f
Groofi <varchikcreed@mail.ru>
2020-11-29 20:08:13 +0300

    Home_3

--- shop_django/urls.py
+++ shop_django/urls.py
@@ -14,17 +14,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from mainapp import views as mainapp
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', mainapp.main, name="main" ),
-    path('products/', mainapp.products, name="products"),
-    path('products/all/', mainapp.products_all, name="products_all"),
-    path('products/home/', mainapp.products_home, name="products_home"),
-    path('products/office/', mainapp.products_office, name="products_office"),
-    path('products/modern/', mainapp.products_modern, name="products_modern"),
-    path('products/classic/', mainapp.products_classic, name="products_classic"),
+    path('', mainapp.main, name="main"),
+    path('products/', include("mainapp.urls", namespace="products")),
     path('contacts/', mainapp.contact, name="contacts"),
     path('admin/', admin.site.urls),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
